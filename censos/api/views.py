a2d73dc3420dc538850584263df716b9afc2c6ef
miguel ternero <migteralg@alum.us.es>
2017-12-16 18:51:43 +0100

    update1

--- censos/api/views.py
+++ censos/api/views.py
@@ -3,7 +3,7 @@ from .serializers import CensoSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-
+from datetime import datetime
 
 @api_view(['GET'])
 def get_censo(request):
@@ -27,16 +27,27 @@ def can_vote(request):
 
 
 def create_censo(request):
+    Censo.object.create(id_votacion='33',id_grupo='21',nombre='prueba',fecha_ini=datetime.now(),fecha_fin='')
     return None
 
 
+@api_view(['GET'])
 def update_censo(request):
-    id = request.GET.get('id')
-    nombre=request.GET.get('nombre')
-    fechafin = request.GET.get('fecha_fin')
-    if id and Censo.obects.filter(id=id).exists():
-       censo = Censo.objets.filter(id=id).update(nombre=nombre, fecha_ini=time.strftime('%X'), fecha_fin=fechafin)
-
+    id = request.GET.get('id', '')
+    nombre=request.GET.get('nombre', '')
+    print(id, nombre)
+    # fechaini = request.GET.get('fecha_ini', '')
+    # if not fechaini:
+    #     fechaini = datetime.now().strftime('%d/%m/%a')
+    # fechafin = request.GET.get('fecha_fin', '')
+    censo = None
+    if id and Censo.objects.filter(id=id).exists():
+        try:
+             Censo.objects.filter(id=id).update(nombre=nombre)
+             censo = Censo.objects.get(id=id)
+        except Exception as e:
+            print(str(e))
+            return Response(status=status.HTTP_404_NOT_FOUND)
     context = {'request': request}
     serializer = CensoSerializer(censo, context=context)
     return Response(serializer.data)
