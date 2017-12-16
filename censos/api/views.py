b15d2b8c0730e62a993fa36e90e60ebb32433d00
maria ruiz <marikichi1996@gmail.com>
2017-12-16 18:48:40 +0100

    filter2

--- censos/api/views.py
+++ censos/api/views.py
@@ -18,20 +18,35 @@ def get_censo(request):
     return Response(serializer.data)
 
 
-@api_view(['filter'])
+@api_view(['GET'])
 def filter_censos(request):
     nombre = request.GET.get('nombre', '')
     fecha_ini = request.GET.get('fecha_ini', '')
     fecha_fin = request.GET.get('fecha_fin', '')
     id_votacion = request.GET.get('id_votacion', '')
     id_grupo = request.GET.get('id_grupo', '')
+
     try:
-        user = Censo.objects.filter(nombre=nombre, fecha_fin=fecha_fin, fecha_ini=fecha_ini, id_votacion=id_votacion, id_grupo=id_grupo)
-    except Censo.doesNotExist:
+        if nombre and fecha_ini and fecha_fin and id_votacion and id_grupo:
+            censos = Censo.objects.filter(nombre=nombre, fecha_fin=fecha_fin, fecha_ini=fecha_ini, id_votacion=id_votacion, id_grupo=id_grupo)
+        """elif nombre and not fecha_ini and not fecha_fin and not id_votacion and not id_grupo:
+            censos = Censo.objects.filter(nombre=nombre)
+        elif nombre and fecha_ini and not fecha_fin and not id_votacion and not id_grupo:
+            censos = Censo.objects.filter(nombre=nombre, fecha_ini=fecha_ini)
+        elif nombre and fecha_ini and fecha_fin and not id_votacion and not id_grupo:
+            censos = Censo.objects.filter(nombre=nombre, fecha_ini=fecha_ini, fecha_fin=fecha_fin)
+        elif nombre and fecha_ini and fecha_fin and id_votacion and not id_grupo:
+            censos = Censo.objects.filter(nombre=nombre, fecha_ini=fecha_ini, fecha_fin=fecha_fin, id_votacion=id_votacion)
+        elif not nombre and fecha_ini and fecha_fin and id_votacion and  id_grupo:
+            censos = Censo.objects.filter(fecha_ini=fecha_ini, fecha_fin=fecha_fin, id_votacion=id_votacion, id_grupo=grupo)
+        elif not nombre and not fecha_ini and fecha_fin and id_votacion and id_grupo:
+            censos = Censo.objects.filter(fecha_fin=fecha_fin, id_votacion=id_votacion, id_grupo=grupo)
+        elif not nombre and not fecha_ini and not fecha_fin"""
+    except Exception:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     context = {'request': request}
-    serializer = CensoSerializer(user, context=context)
+    serializer = CensoSerializer(censos, context=context, many=True)
     return Response(serializer.data)
 
 
@@ -40,6 +55,7 @@ def can_vote(request):
 
 
 def create_censo(request):
+
     return None
 
 
