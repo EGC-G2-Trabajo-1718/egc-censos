fc71a0673e0ae30d67a9d657164ae2d79ebe6da4
KirinAnks <fjgparrales@gmail.com>
2017-12-21 16:41:12 +0100

    Editado el modelo Censo con atributo ROL para el Issue de getUsersByRole de Autenticación

--- censos/api/models.py
+++ censos/api/models.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 # Create your models here.
 class Censo(models.Model):
     id_votacion = models.IntegerField(blank=False, null=False)
-    id_grupo = models.IntegerField(blank=False, null=False)
+    #  Campo ROLE, con valores de 'ASISTENTE', 'PONENTE', 'AMBOS': antiguo id_grupo, cambiado por API de Autenticación
+    rol = models.CharField(max_length=20, blank=False, null=False, default='ASISTENTE')
     nombre = models.CharField(max_length=50, blank=False, null=False)
     fecha_ini = models.DateTimeField(default=timezone.now, blank=True, null=False)
     fecha_fin = models.DateTimeField(blank=True, null=False)
--- censos/api/serializers.py
+++ censos/api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class CensoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Censo
-        fields = ('id_votacion', 'id_grupo', 'nombre', 'fecha_ini', 'fecha_fin')
+        fields = ('id', 'id_votacion', 'rol', 'nombre', 'fecha_ini', 'fecha_fin')
 
     fecha_ini = serializers.DateTimeField(format='%d/%m/%Y %H:%M')
     fecha_fin = serializers.DateTimeField(format='%d/%m/%Y %H:%M')
