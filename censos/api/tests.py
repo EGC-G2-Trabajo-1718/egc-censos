323371886e531d33ae9e81b7eac49f2e53543981
miguel ternero <migteralg@alum.us.es>
2018-01-04 11:04:19 +0100

    Comienzotest

--- censos/api/tests.py
+++ censos/api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . import views
+
+class CensosTest(TestCase):
+    def UpdateTest(self):
+        views.update_censo.
+
+    def UpdateTest(self):
+
+        
+
 
 
