aba8cef3f549358100dbca371d41c1cb621b327e
daniellp20 <dany15-paradas@hotmail.com>
2018-01-08 20:25:13 +0100

    Modificado archivo .gitignore

--- censos/api/tests.py
+++ censos/api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Censo
+from rest_framework.test import APITestCase
+from . import views
+
+
+
+class CensoTests(APITestCase):
+
+    def test_delete_censo(self):
+        c1 = Censo.objects.create(id_votacion=196, id_grupo=101, nombre="Censocreate", fecha_ini="2017-12-15 11:11:11", fecha_fin="2018-12-15 11:11:11")
+        c2 = Censo.objects.get(nombre="Censocreate")
+        Censo.objects.filter(nombre="Censocreate").delete()
+
+
+
+
+
+
 
 
--- censos/api/urls.py
+++ censos/api/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^get/', views.get_censo),
+    url(r'^get/', views.get_censo, name='get'),
     url(r'^filter/', views.filter_censos),
     url(r'^can_vote/', views.can_vote),
     url(r'^create/', views.create_censo),
     url(r'^update/', views.update_censo),
-    url(r'^delete/', views.delete_censo)
+    url(r'^delete/', views.delete_censo, name='delete')
 ]
