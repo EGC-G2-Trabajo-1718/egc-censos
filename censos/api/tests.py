529e95c18d8b3b6d7c0fa5277d1e62fc843305aa
daniellp20 <dany15-paradas@hotmail.com>
2018-01-09 13:48:23 +0100

    fix: Correcion de un test encargado de comprobar la funcion de delete En el fichero tests.py habia un test llamado test_delete_censo que se encargaba de comprobar que la funcion de eliminar era correcta, esto se añadio en un anterior commit pero este test no realizaba todas las comprobaciones pertinentes

--- censos/api/tests.py
+++ censos/api/tests.py
@@ -1,25 +1,14 @@
-from django.test import TestCase
-from .models import Censo
 from rest_framework.test import APITestCase
-from . import views
-
+from .models import Censo
 
 
 class CensoTests(APITestCase):
-
     def test_delete_censo(self):
-        c1 = Censo.objects.create(id_votacion=196, id_grupo=101, nombre="Censocreate", fecha_ini="2017-12-15 11:11:11", fecha_fin="2018-12-15 11:11:11")
-        c2 = Censo.objects.get(nombre="Censocreate")
-        Censo.objects.filter(nombre="Censocreate").delete()
-
-
-
-
-
-
-
-
-
-
-
-
+        c1 = Censo.objects.create(id_votacion=196, id_grupo=101, fecha_ini='2017-11-15 11:11:11', nombre='Censocreate',
+                                  fecha_fin='2018-12-15 11:11:11')
+        c1.save()
+        c2 = Censo.objects.all()
+        self.assertEquals(c2.count(), 1)
+        Censo.objects.filter(id=c1.id).delete()
+        c3 = Censo.objects.all()
+        self.assertEquals(c3.count(), 0)
