55a87e180719fc36247d19d3f4fe617ba90f73a6
daniellp20 <dany15-paradas@hotmail.com>
2018-01-11 17:19:45 +0100

    Corregido de nuevo el test Se han añadido test positivo y negativo ademas las comprobaciones son mas fuertes

--- censos/api/tests.py
+++ censos/api/tests.py
@@ -2,19 +2,38 @@ from django.test import TestCase
 from . import views
 from rest_framework.test import APITestCase
 from .models import Censo
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
+import json
 
 
 class CensoTests(APITestCase):
-    def test_delete_censo(self):
+    def test_delete_censo_positive(self):
         c1 = Censo.objects.create(id_votacion=196, rol='ASISTENTE', fecha_ini='2017-11-15 11:11:11',
                                   nombre='Censocreate',
                                   fecha_fin='2018-12-15 11:11:11')
-        c1.save()
-        c2 = Censo.objects.all()
-        self.assertEquals(c2.count(), 1)
+
+        self.assertEquals(Censo.objects.filter(nombre='Censocreate').exists(), True)
+
         Censo.objects.filter(id=c1.id).delete()
-        c3 = Censo.objects.all()
-        self.assertEquals(c3.count(), 0)
+        self.assertEquals(Censo.objects.filter(nombre='Censocreate').exists(), False)
+        return Censo.objects.filter(nombre='Censocreate').exists() is True
+
+
+    def test_delete_censo_negative(self):
+        censo = Censo.objects.create(id_votacion=200, rol='ASISTENTE', nombre='', fecha_ini='2017-11-15 11:11:11',
+                                     fecha_fin='2018-12-15 11:11:11')
+        censo.save()
+
+        self.assertEquals(Censo.objects.filter(id=censo.id).exists(),True)
+        Censo.objects.filter(id=censo.id, nombre='nombre_que_no_existe').delete()
+        self.assertEquals(Censo.objects.filter(id=censo.id).exists(),True)
+
+        return Censo.objects.filter(id=censo.id).exists() is True
+
+
+
 
     def test_filter_(self):
         c1 = Censo.objects.create(id_votacion=196, rol='ASISTENTE', fecha_ini='2017-11-15 11:11:11',
