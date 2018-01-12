dc7370c849e156cfd3a3a5ac180ed6cf5c74df54
daniellp20 <dany15-paradas@hotmail.com>
2018-01-12 19:02:24 +0100

    Corregido test antiguos y hecho un test positivo y un negativo mas

--- censos/api/tests.py
+++ censos/api/tests.py
@@ -9,28 +9,53 @@ import json
 
 
 class CensoTests(APITestCase):
-    def test_delete_censo_positive(self):
+    def test_delete_censo_positiv0_0(self):
         c1 = Censo.objects.create(id_votacion=196, rol='ASISTENTE', fecha_ini='2017-11-15 11:11:11',
                                   nombre='Censocreate',
                                   fecha_fin='2018-12-15 11:11:11')
 
         self.assertEquals(Censo.objects.filter(nombre='Censocreate').exists(), True)
-
         Censo.objects.filter(id=c1.id).delete()
         self.assertEquals(Censo.objects.filter(nombre='Censocreate').exists(), False)
-        return Censo.objects.filter(nombre='Censocreate').exists() is True
 
-
-    def test_delete_censo_negative(self):
+    def test_delete_censo_negativo_0(self):
+        excepcion = False
         censo = Censo.objects.create(id_votacion=200, rol='ASISTENTE', nombre='', fecha_ini='2017-11-15 11:11:11',
                                      fecha_fin='2018-12-15 11:11:11')
-        censo.save()
 
-        self.assertEquals(Censo.objects.filter(id=censo.id).exists(),True)
-        Censo.objects.filter(id=censo.id, nombre='nombre_que_no_existe').delete()
-        self.assertEquals(Censo.objects.filter(id=censo.id).exists(),True)
+        try:
+            Censo.objects.filter(id='').delete()
+        except:
+            excepcion = True
 
-        return Censo.objects.filter(id=censo.id).exists() is True
+        self.assertEquals(excepcion, True)
+
+    def test_delete_censo_positivo_1(self):
+        c2 = Censo.objects.create(id_votacion=196, rol='ASISTENTE', fecha_ini='2017-11-15 11:11:11',
+                                  nombre='Censocreate1',
+                                  fecha_fin='2018-12-15 11:11:11')
+        c3 = Censo.objects.create(id_votacion=201, rol='ASISTENTE', fecha_ini='2017-10-01 09:09:09',
+                                  nombre='Censocreate2', fecha_fin='2018-05-05 05:05:05')
+
+        conjunto = Censo.objects.all()
+        self.assertEquals(conjunto.count(), 2)
+        Censo.objects.filter(id=c2.id).delete()
+        conjunto1 = Censo.objects.all()
+        self.assertEquals(conjunto1.count(), 1)
+        Censo.objects.filter(id=c3.id).delete()
+        conjunto2 = Censo.objects.all()
+        self.assertEquals(conjunto2.count(), 0)
+
+    def test_delete_censo_negativo_1(self):
+
+        censo1 = Censo.objects.create(id_votacion=200, rol='ASISTENTE', nombre='', fecha_ini='2017-11-15 11:11:11',
+                                     fecha_fin='2018-12-15 11:11:11')
+
+        conjunto = Censo.objects.all()
+        self.assertEquals(conjunto.count(), 1)
+        Censo.objects.filter(id=35252).delete()
+        conjunto1 = Censo.objects.all()
+        self.assertEquals(conjunto.count(), 1)
 
 
 
